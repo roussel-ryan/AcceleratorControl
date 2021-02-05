fixed normalization transformation and bayesian optimization files

accelerator_interface.py
@@ -11,20 +11,22 @@ import logging
 
 
 class AWAInterface:
-    UseNIFG=False
-    Initialized=False
+    UseNIFG = False
+    Initialized = False
     m_CameraClient = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    m_AWACameraHost="127.0.0.1"
-    m_AWAPGCamPort=2019
-    m_AWANIFGPort=2029
-    NewMeasurement=False
-    FWHMX=10000
-    FMHMY=10000
-    FMHML=10000
-    TempVal=0.0
-    Testing=False
+    m_AWACameraHost = "127.0.0.1"
+    m_AWAPGCamPort = 2019
+    m_AWANIFGPort = 2029
+    NewMeasurement = False
+    FWHMX = 10000
+    FMHMY = 10000
+    FMHML = 10000
+    TempVal = 0.0
+    Testing = False
+
+    
     def __init__(self,UseFrameGrabber=True,Testing=False):
-        self.Testing=Testing
+        self.Testing = Testing
         self.initialize_connections(UseFrameGrabber)
 
     def initialize_connections(self,UseFrameGrabber):
@@ -131,10 +133,10 @@ class AWAInterface:
         
     def test(self, NSamples):
         self.GetNewImage(NSamples)
-        NShots=0
-        r=np.sin(self.TempVal)
-        while (NShots<NSamples):
+        NShots = 0
+        r = np.sin(self.TempVal)
+        while (NShots < NSamples):
             self.FWHMX[NShots] = sum(r)
             self.FWHMY[NShots] = r[0]
             NShots += 1
-        return self.FWHMY
+        return self.FWHMX

controller.py
@@ -48,7 +48,9 @@ class AWAController:
         n_steps = self.config.get('scan_steps',5)
         n_samples = self.config.get('samples', 5)
         
-        self.logger.info(f'starting scan of {parameter_name} with {n_steps} steps and {n_samples} samples per step')
+        self.logger.info(f'starting scan of {parameter_name}'\
+                         'with {n_steps} steps and {n_samples}'\
+                         'samples per step')
 
         X = np.linspace(lower, upper, n_steps).reshape(-1,1)
         
@@ -103,12 +105,20 @@ class AWAController:
                 utils.check_bounds(x_val,p)
             if not self.testing:
                 #self.interface.set_parameters(x,[p.channel for p in parameters])
-                self.logger.info(f'setting parameters {parameter_names} to values {x}') 
+                self.logger.info(
+                    f'setting parameters {parameter_names} to values {x}') 
 
-                self.interface.set_beamline([p.channel for p in parameters],x)
+                self.interface.set_beamline(
+                    [p.channel for p in parameters],x)
+                
             else:
-                self.logger.info(f'Test: setting parameters {parameter_names} to values {x}') 
-                self.interface.set_beamline([p.channel for p in parameters],x)
+                self.logger.info(
+                    f'Test: setting parameters {parameter_names}'\
+                    f' to values {x}')
+                
+                self.interface.set_beamline(
+                    [p.channel for p in parameters],x)
+                
             time.sleep(self.wait_time)
 
             #append new state to data

optimization.py
@@ -4,6 +4,7 @@ import torch
 import parameter
 import observations
 import utilities as utils
+import transformer
 
 from botorch.models import SingleTaskGP
 from botorch.fit import fit_gpytorch_model
@@ -22,7 +23,7 @@ class SingleObjectiveBayesian:
 
     '''
 
-    def __init__(self, parameters, objective, controller, **kwargs):
+    def __init__(self, parameters, objective, controller, maximize = False, **kwargs):
         '''
         Initialize optimizer
 
@@ -37,11 +38,11 @@ class SingleObjectiveBayesian:
         controller : controller.AWAController
              Controller object used to control the accelerator
         
-        minimize : bool
+        minimize : bool, default = True
              If True minimize the objective, otherwise maximize
 
         '''
-        pkey=list(parameters.keys())
+        pkey = list(parameters.keys())
         
         assert isinstance(parameters[pkey[0]], parameter.Parameter)
         assert isinstance(objective, observations.Observation)
@@ -54,30 +55,25 @@ class SingleObjectiveBayesian:
         self.parameter_names = list(self.parameters.keys())
         self.n_parameters = len(self.parameters)
 
+        self.maximize = maximize
         
         #check if data is available
         #Data won't be available until observe of ctrl has been called.
         X, f = self.get_data()
+
         #contruct a GP model
         self.gp = SingleTaskGP(X,f)
 
         self.beta = kwargs.get('beta',0.1)
 
     def optimize(self, n_steps = 10, n_samples = 5):
-        b1=self.parameters[self.parameter_names[0]].bounds
-        b2=self.parameters[self.parameter_names[1]].bounds
-        d1=b1[1]-b1[0]
-        d2=b2[1]-b2[0]
         for i in range(n_steps):
             #get candidate for observation
             candidate = self.max_acqf()
 
-            #unnormalize
-            #candidate = utils.unnormalize(candidate, self.parameters)
-            candidate[0][0] *= d1
-            candidate[0][1] *= d2
-            candidate[0][0] += b1[0]
-            candidate[0][1] += b2[0]
+            #unnormalize candidate
+            candidate = self.controller.tx.backward(candidate.numpy().reshape(1,-1))
+            
             #set parameters
             self.controller.set_parameters(candidate.flatten(), self.parameter_names)
 
@@ -94,7 +90,7 @@ class SingleObjectiveBayesian:
         
     def max_acqf(self):
         #finds new canidate point based on UCB acquisition function
-        UCB = UpperConfidenceBound(self.gp, self.beta)
+        UCB = UpperConfidenceBound(self.gp, self.beta, maximize = self.maximize)
         bounds = torch.stack([torch.zeros(self.n_parameters),
                               torch.ones(self.n_parameters)])
         candidate, acq_value = optimize_acqf(UCB, bounds = bounds,
@@ -109,7 +105,7 @@ class SingleObjectiveBayesian:
                                          self.gp)
         fit_gpytorch_model(mll)
         
-    def get_data(self, normalize = False):
+    def get_data(self, normalize = True):
         '''
         return numpy array with observations and convert to torch
         Note: by default input parameters are normalized from 0 to 1
@@ -119,22 +115,19 @@ class SingleObjectiveBayesian:
 
         '''
 
-        f = self.controller.data[self.objective.name]
-        #X = self.controller.data[
-        #    [p.name for p in self.parameters]]
+        f = self.controller.data[self.objective.name].to_numpy().reshape(-1,1)
         X = self.controller.data[
-            [p for p in self.parameter_names]]
-
-#        X = torch.Tensor(X.to_numpy())
-        print(X)
-        X = torch.Tensor(X.to_numpy())
-        f = torch.Tensor(f.to_numpy())
-
+            [p for p in self.parameter_names]].to_numpy()
+        
         if normalize:
             #standardize f
-            f = transforms.standardize(f)
-            X = utils.normalize(X, self.parameters)
-        f = f.reshape(-1,1)
+            tf = transformer.Transformer(f)
+            f = tf.forward(f)
+            X = self.controller.tx.forward(X)
+        
+        X = torch.from_numpy(X)
+        f = torch.from_numpy(f)
+
             
         return X, f
                                                     