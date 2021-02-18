fixed issues with scaling and normalization, must specify unnormalized reference point now in mobo object creation

accelerator_control/mobo.py
@@ -9,6 +9,7 @@ from . import transformer
 from botorch.models import SingleTaskGP, FixedNoiseGP
 from botorch.fit import fit_gpytorch_model
 from gpytorch.mlls import ExactMarginalLogLikelihood
+from gpytorch.means import ConstantMean
 
 from botorch.acquisition.multi_objective.analytic import ExpectedHypervolumeImprovement
 from botorch.utils.multi_objective.box_decomposition import NondominatedPartitioning
@@ -22,7 +23,7 @@ class MultiObjectiveBayesian:
 
     '''
 
-    def __init__(self, parameters, objectives, controller, **kwargs):
+    def __init__(self, parameters, objectives, controller, ref, **kwargs):
         '''
         Initialize optimizer
 
@@ -54,7 +55,17 @@ class MultiObjectiveBayesian:
         self.parameter_names = parameters
         self.n_parameters = len(self.parameters)
         self.n_objectives = len(self.objectives)
-        
+    
+        #reference point - assumes minimization where the minimum value for both objectives is zero
+        assert ref.shape[0] == self.n_objectives
+        self.ref = ref
+                
+        #define function transformers for each objective
+        self.transformers = []
+        for i in range(self.n_objectives):
+            bound_vals = np.array((0.0,self.ref[i])).reshape(2,1)
+            self.transformers += [transformer.Transformer(bound_vals)]
+
         #check if data is available
         #Data won't be available until observe of ctrl has been called.
         self.X, self.f = self.get_data()
@@ -62,12 +73,23 @@ class MultiObjectiveBayesian:
         #contruct a GP model - Matern kernel w/ nu = 2.5 and ARD, GammaPriors on lengthscales and output scales
         #note: pass a custom kernel via the covar_module argument
         #note: objectives are multiplied by -1 to do minimization
-        self.gp = SingleTaskGP(self.X, self.f)
+        #self.gp = SingleTaskGP(self.X, self.f)
+
+        #set mean module variable to -1 to improve optimization and set gradient to false
+        #self.gp.mean_module.constant.data = torch.tensor((-1.0,-1.0))
+        #self.gp.mean_module.constant.requires_grad = False
 
         
     def optimize(self, n_steps = 10, n_samples = 5):
         for i in range(n_steps):
+            #get data and retrain gp
+            self.X, self.f = self.get_data()
             
+            self.gp = SingleTaskGP(self.X, self.f)
+            #self.gp.mean_module.constant.requires_grad = False
+            self.fit_gp()
+
+   
             #get candidate for observation and unnormalize
             candidate = self.max_acqf().numpy().flatten()
             unnormed_candidate = np.zeros_like(candidate)
@@ -95,18 +117,13 @@ class MultiObjectiveBayesian:
             for obs in observations:
                 self.controller.observe(obs, n_samples)
 
-            #get data and retrain gp
-            self.X, self.f = self.get_data()
-            
-            self.gp = SingleTaskGP(self.X, self.f)
-            self.fit_gp()
             
         
         
     def max_acqf(self):
         #finds new canidate point based on EHVI acquisition function
         partitioning = NondominatedPartitioning(2, Y = self.f)
-        EHVI = ExpectedHypervolumeImprovement(self.gp, -1.0 * torch.ones(2), partitioning)
+        EHVI = ExpectedHypervolumeImprovement(self.gp, -1.0 * self.ref, partitioning)
 
         bounds = torch.stack([torch.zeros(self.n_parameters),
                               torch.ones(self.n_parameters)])
@@ -120,6 +137,7 @@ class MultiObjectiveBayesian:
         #fits GP model
         mll = ExactMarginalLogLikelihood(self.gp.likelihood,
                                          self.gp)
+
         fit_gpytorch_model(mll)
         
     def get_data(self, normalize = True):
@@ -131,26 +149,25 @@ class MultiObjectiveBayesian:
         Always use normalize = True unless doing visualization!!!
 
         '''
-        data = self.controller.data.fillna(-np.inf).groupby(['state_idx']).max()
+        data = self.controller.data.groupby(['state_idx']).mean()
         
         f = data[[obj.name for obj in self.objectives]]
         f = f.to_numpy()
         X = data[[p.name for p in self.parameters]].to_numpy()
         
         if normalize:
-            #standardize f
-            tf = transformer.Transformer(f)
-            f = tf.forward(f)
+            #normalize f according to reference point
+            f_normed = np.zeros_like(f)
+            for i in range(self.n_objectives):
+                f_normed[:,i] = self.transformers[i].forward(f[:,i].reshape(-1,1)).flatten()
 
             #normalize each input vector
             X_normed = np.zeros_like(X)
             for i in range(self.n_parameters):
                 X_normed[:,i] = self.parameters[i].transformer.forward(X[:,i].reshape(-1,1)).flatten()
             
-            #X = self.controller.tx.forward(X)
         
         X = torch.from_numpy(X_normed)
-        f = -1 * torch.from_numpy(f)
-
+        f = -1 * torch.from_numpy(f_normed)
             
         return X, f

accelerator_control/observations.py
@@ -12,16 +12,22 @@ class Observation:
     Observation function class that keeps track of function name and callable 
     function to do the observation.
     
-    If this observation can be made simultaneously with other observations using the same process 
-    pass it to a GroupObservation.add_child(). By default a observation has no parent =(.
-    An example of a simultaneous observation is getting the x and y beamsize from a single image.
+    If this observation can be made simultaneously with other observations 
+    using the same process pass it to a GroupObservation.add_child(). 
+    By default a observation has no parent =(.
+    
+    An example of a simultaneous observation is getting the x and y beamsize 
+    from a single image.
 
-    If is_child is true, calls to this observation will execute the group callable method instead. 
-    This is done so that optimizers can call individual observations while still collecting 
-    excess data during optimization. 
+    If is_child is true, calls to this observation will execute the 
+    group callable method instead. 
+    
+    This is done so that optimizers can call individual observations while 
+    still passively collecting excess data during optimization. 
 
     For use, overwrite __call__().
-    Observation callables return a pandas DataFrame object with column names = to observation name.
+    Observation callables return a pandas DataFrame object with 
+    column names = to observation name.
     
     Arguments
     ---------
@@ -34,10 +40,11 @@ class Observation:
         self.is_child = False
         
     def add_parent(self, parent):
+        '''adds parent to observation -> self._call__ is not necessary'''
+        
         self.is_child = True
         self.parent = parent
             
-        #self.name = '.'.join((self.parent.name, self.name))
         
     def __call__(self, controller):
         '''
@@ -75,6 +82,12 @@ class GroupObservation:
         
         
     def __call__(self, controller):
+        '''
+        
+        impliments observation procedure, 
+        should return a pandas DataFrame object
+
+        '''
         raise NotImplementedError
 
     def add_child(self, child):
@@ -106,7 +119,7 @@ class OTR2Profiles(GroupObservation):
         xrms = controller.interface.get_PYs([otr_base_pv + 'XRMS'])
         yrms = controller.interface.get_PYs([otr_base_pv + 'YRMS'])
 
-        if measure_z:
+        if self.measure_z:
             controller.interface.set_TCAV(1)
             tcav_on_xrms = controller.interface.get_PYs([otr_base_pv + 'XRMS'])
             controller.interface.set_TCAV(0)
@@ -119,7 +132,7 @@ class OTR2Profiles(GroupObservation):
         else:
             results = np.array((sigma_x,sigma_y))
             
-        return pd.DataFrame(data = results,
+        return pd.DataFrame(data = results.reshape(1,-1),
                             columns = self.output_names)
         
         
@@ -129,25 +142,20 @@ class TestSOBO(Observation):
         super().__init__(name)
 
     def __call__(self, controller):
-        val = controller.interface.test_sobo().reshape(1,1)
-        return pd.DataFrame(val,
+        val = controller.interface.test_observation()
+        return pd.DataFrame(val[0].reshape(1,1),
                             columns = [self.name])
         
 class TestMOBO(GroupObservation):
     '''observation class used for testing MOBO algorithm'''
     def __init__(self, name = 'TestMOBO'):
-        self.output_names = ['1','2']
-        super().__init__(name)
-
-        #add children observations
-        for name in self.output_names:
-            obs = Observation(name)
-            self.add_child(obs)
+        output_names = ['1','2']
+        super().__init__(name, output_names)
 
     def __call__(self, controller):
-        vals = controller.interface.test_mobo()
-        return pd.DataFrame(np.array(vals).reshape(1,-1),
-                            columns = self.get_children_names())
+        vals = controller.interface.test_observation()
+        return pd.DataFrame(vals.reshape(1,-1),
+                            columns = self.output_names)
         
      
 

test.py
@@ -1,30 +1,10 @@
 import numpy as np
 import logging
 import torch
-from accelerator_control import controller, observations, sobo, mobo, interface
+from accelerator_control import controller, observations, sobo, mobo, interface, parameter
 
 import matplotlib.pyplot as plt
 
-def main():
-    logging.basicConfig(level=logging.INFO)
-    obs = observations.Observation("obs1")
-    image_save = observations.ImageSave('pics')
-
-    #define interface and controller
-    interf = interface.AWAInterface(True, True)
-    c = controller.AWAController('test.json', interf)
-
-    c.set_parameters(torch.Tensor(np.array((3,4))),['FocusingSolenoid', 'BuckingSolenoid'])
-    #c.do_scan('FocusingSolenoid', 20, 50, obs)
-    c.observe(obs, 5)
-
-    opt_params = c.parameters
-    opt_obj = obs
-    opt = optimization.SingleObjectiveBayesian(opt_params, opt_obj, c)
-    opt.optimize()
-    #opt.optimize()
-
-    print(c.data)
     
 #main()
 logging.basicConfig(level=logging.INFO)
@@ -32,9 +12,10 @@ sobo_obs = observations.TestSOBO()
 #obs = observations.AWABeamSize()
 #image_save = observations.ImageSave('pics')
 
-c = controller.AWAController('test.json',True)
 
-opt_params = c.get_parameters(['FocusingSolenoid', 'BuckingSolenoid'])
+c = controller.Controller('test.json', interface.TestInterface())
+
+opt_params = c.get_named_parameters(['FocusingSolenoid', 'BuckingSolenoid'])
 
 #c.do_scan('FocusingSolenoid', 20, 50, obs)
 
@@ -46,19 +27,23 @@ opt_params = c.get_parameters(['FocusingSolenoid', 'BuckingSolenoid'])
 
 #MOBO test
 mobo_obs = observations.TestMOBO()
-for i in range(10):
+for i in range(20):
     c.set_parameters(opt_params, np.random.rand(2))
     c.observe(sobo_obs)
     c.observe(mobo_obs)
 
 print(c.data)
 
-objs = [sobo_obs, mobo_obs.children[0]]
-opt = mobo.MultiObjectiveBayesian(opt_params, objs, c) 
-opt.optimize(35,1)
+objs = [sobo_obs, mobo_obs.children[1]]
+#objs = mobo_obs.children
+ref = torch.tensor((1.0,5.0))
+opt = mobo.MultiObjectiveBayesian(opt_params, objs, c, ref) 
+opt.optimize(25,5)
 print(c.data)
+
+#print(opt.gp.state_dict())
 
 
 #plot to test
-c.group_data().plot('TestMOBO.1','TestSOBO','scatter')
+c.group_data().plot('1','2','scatter')
 plt.show()