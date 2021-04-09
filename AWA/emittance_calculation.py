fixed emittance calculation and other bugfixes

AWA/emittance_calculation.py
@@ -45,13 +45,15 @@ def calculate_emittance(image, scale, slit_sep, drift):
         ax[1].imshow(image)
         print(image)
     logger.debug(f'triangle_threshold: {triangle_threshold}')
-    proj = np.where(orig_proj > triangle_threshold, orig_proj, 0)
+    proj = np.where(orig_proj > triangle_threshold + 0.05, orig_proj, 0)
+    #proj = orig_proj
 
     #plot proj if in debugging
     if 0:
-        fig,ax = plt.subplots(1,2)
-        ax[0].plot(orig_proj)
-        ax[1].plot(proj)
+        fig,ax = plt.subplots(1,3)
+        ax[0].imshow(image)
+        ax[1].plot(orig_proj)
+        ax[2].plot(proj)
         
         plt.show()
 
@@ -94,19 +96,24 @@ def calculate_emittance(image, scale, slit_sep, drift):
     sorted_idx = np.argsort(b)
     a,b,c = a[sorted_idx],b[sorted_idx],c[sorted_idx]
 
+    logger.debug(f'mean beamlet with_px: {np.mean(c)}')
+    logger.debug(a)
+    logger.debug(b)
+    logger.debug(c)
     #convert pixel lengths to meters
     b = b * scale
     c = c * scale
 
     #define slit locations
     x_slit_m = np.linspace(-(n_blobs-1)/2*slit_sep, (n_blobs-1)/2*slit_sep, n_blobs)
-
+    logger.debug(x_slit_m)
     #calculate beam centroid at slits
     ixi = np.sum(a * x_slit_m) / np.sum(a)
 
     #center b coords on beam center on screen
     b = b - np.average(b, weights = a)
-
+    logger.debug(b)
+    
     #calculate rms spread at slits
     ixxi = np.sum(a * (x_slit_m - ixi)**2) / np.sum(a)
 
@@ -120,7 +127,8 @@ def calculate_emittance(image, scale, slit_sep, drift):
     sp = c / drift
 
     #calc rms divergence at slits
-    ixpxpi = np.sum(a * sp**2 + a * (xp - ixpi)**2) / np.sum(a)
+    ixpxpi = np.sum(a * sp**2) / np.sum(a)
+    #ixpxpi = np.sum(a * sp**2 + a * (xp - ixpi)**2) / np.sum(a)
 
     #calc correlation term at slits
     ixxpi = (np.sum(a * x_slit_m * xp) - np.sum(a) * ixi *ixpi) / np.sum(a)
@@ -133,10 +141,11 @@ def calculate_emittance(image, scale, slit_sep, drift):
     logger.debug(f'ixxpi:{ixxpi}')
 
     #calculate emittance
-    emittance = np.sqrt(ixxi * ixpxpi - ixxpi**2)
+    #emittance = np.sqrt(ixxi * ixpxpi - ixxpi**2)
+    emittance = np.sqrt(ixxi * ixpxpi)
     logger.info(f'calculated emittance: {emittance:.2e}, n_peaks:{len(peaks)}')
 
-    return emittance
+    return emittance, ixxi, ixpxpi, ixxpi
 
 
 if __name__ == '__main__':

AWA/image_processing.py
@@ -22,15 +22,18 @@ def weighted_std(values, weights):
     variance = np.average((values-average)**2, weights=weights)
     return np.sqrt(variance)
 
-def process_and_fit(image, min_size = 500):
+def process_and_fit(image, min_size = 800):
     logger = logging.getLogger(__name__)
     
     
     smoothed_image = filters.gaussian(image, 3)
-    
+
     triangle_threshold = filters.threshold_triangle(smoothed_image)
     logger.debug(f'triangle_threshold: {triangle_threshold}')
-    thresholded_image = np.where(smoothed_image > triangle_threshold * 1.0, 1, 0)
+    thresholded_image = np.where(smoothed_image > triangle_threshold, 1, 0)
+    smoothed_image = np.where(smoothed_image > triangle_threshold, 
+                              smoothed_image,0)
+    #thresholded_image = np.where(smoothed_image > 0, 1, 0)
     
     thresholded_image = remove_small_blobs(thresholded_image, min_size)
     thresholded_image = thresholded_image.astype(np.uint8)
@@ -42,7 +45,9 @@ def process_and_fit(image, min_size = 500):
     #fit ellipses
     ellipses = []
     for cnt in cnts:
-        if cv2.contourArea(cnt) > min_size:
+        blob_area = cv2.contourArea(cnt)
+        logger.debug(f'blob area: {blob_area}')
+        if  blob_area > min_size:
             ellipse = cv2.fitEllipse(cnt)
             ellipses += [ellipse]
 
@@ -58,7 +63,7 @@ def process_and_fit(image, min_size = 500):
         fig.colorbar(c)
         plt.show()
     
-    return thresholded_image, n_blobs, ellipses
+    return thresholded_image, smoothed_image, n_blobs, ellipses
 
 def check_image(image, verbose = False, n_blobs_required = 1):
     '''
@@ -72,10 +77,10 @@ def check_image(image, verbose = False, n_blobs_required = 1):
 
     '''
     logger = logging.getLogger(__name__)
-    image, n_blobs, ellipses = process_and_fit(image)
+    timage, simage, n_blobs, ellipses = process_and_fit(image)
 
    
-    if np.count_nonzero(image > 65400) > 500 :
+    if np.count_nonzero(simage > 65400) > 200 :
         logger.warning(f'image saturated')
         return 0
     
@@ -85,8 +90,8 @@ def check_image(image, verbose = False, n_blobs_required = 1):
     
         
     #get the projected std of both x and y
-    proj_x = np.sum(image, axis = 0)
-    proj_y = np.sum(image, axis = 1)
+    proj_x = np.sum(simage, axis = 0)
+    proj_y = np.sum(simage, axis = 1)
     
     #calculate stds
     x_len = len(proj_x)
@@ -94,18 +99,13 @@ def check_image(image, verbose = False, n_blobs_required = 1):
     std_x = weighted_std(np.arange(x_len), proj_x)
     std_y = weighted_std(np.arange(y_len), proj_y)
     
-    std_scale = 2.5
-    side_scale = 0.65
-    logger.debug(std_x*std_scale)
-    logger.debug(x_len * side_scale)
-    logger.debug(std_y*std_scale)
-    logger.debug(y_len * side_scale)
-    if (std_x*std_scale > side_scale* x_len) or (std_y*std_scale > side_scale*y_len):
+    len_factor = 5
+    if (std_x*len_factor > x_len ) or (std_y*len_factor > y_len):
         logger.warning('beam too diffuse')
         return 0
     
     #if there is no beam on the edges
-    if zero_surrounded(image):
+    if zero_surrounded(timage):
         return 1
     else:
         logger.warning('ROI is clipping the beam envelope')
@@ -126,11 +126,11 @@ def remove_small_blobs(image, min_size):
     
 def rotate_beamlets(image):
     logger = logging.getLogger(__name__)
-    image, n_blobs, ellipses = process_and_fit(image, min_size = 500)
+    timage, simage, n_blobs, ellipses = process_and_fit(image, min_size = 500)
     
     
     mean_angle = np.mean(np.array([ele[-1] for ele in ellipses]))
-    rotated_image = transform.rotate(image, mean_angle - 90.0)
+    rotated_image = transform.rotate(simage, mean_angle - 90.0)
     
     return rotated_image, mean_angle - 90.0, n_blobs
     

AWA/interface.py
@@ -167,36 +167,38 @@ class AWAInterface(interface.AcceleratorInterface):
                     self.USBDIO.SetReadyState(2,1)
                     
                     #check charge on ICT1 is within bounds or charge bounds is not specified (target charge < 0)
-                    #ICT1_charge = caget(f'AWAICTMon:Ch1')
-                    ICT1_charge = 0
-                    if (np.abs(ICT1_charge - target_charge) < charge_deviation * target_charge) or (target_charge < 0):
+                    ICT1_charge = caget(f'AWAICTMon:Ch1')
+                    if (np.abs(ICT1_charge - target_charge) < np.abs(charge_deviation * target_charge)) or (target_charge < 0):
                         
                         a = self.m_CameraClient.recv(1024)
                         #print(a)
                         b = "".join(chr(x) for x in a)
-
-                        c = eval(b)
+                        try:
+                            c = eval(b)
                     
-                        self.FWHMX[NShots] = c['FWHMX']
-                        self.FWHMY[NShots] = c['FWHMY']
-                        self.FWHML[NShots] = c['FWHML']
-                        self.CentroidX[NShots] = c['CX']
-                        self.CentroidY[NShots] = c['CY']
-                        self.NewMeasurement = True
-                        self.img += [self.GetImage()]
+                            self.FWHMX[NShots] = c['FWHMX']
+                            self.FWHMY[NShots] = c['FWHMY']
+                            self.FWHML[NShots] = c['FWHML']
+                            self.CentroidX[NShots] = c['CX']
+                            self.CentroidY[NShots] = c['CY']
+                            self.NewMeasurement = True
+                            self.img += [self.GetImage()]
+                            
+                            #get charge
+                            for i in range(1,5):
+                                self.charge[NShots, i - 1] = caget(f'AWAICTMon:Ch{i}')
+    
+                            #get ROI
+                            ROI = self.GetROI()
+                            
+                            self.logger.debug(ROI)
                         
-                        #get charge
-                        for i in range(1,5):
-                            self.charge[NShots, i - 1] = caget(f'AWAICTMon:Ch{i}')
+                            NShots += 1
 
-                        #get ROI
-                        ROI = self.GetROI()
-                        
-                        self.logger.debug(ROI)
-                        
-                        NShots += 1
-
-
+                        except SyntaxError:
+                            self.logger.warning('sleeping!')
+                            time.sleep(1)
+                            
                         
                     else:
                         #if we are considering charge limits then print a warning

AWA/observations.py
@@ -198,7 +198,7 @@ class Emittance(AWAScreen):
         self.m_per_px = 25.4e-3 / screen_width_px
             
         super().__init__(name = 'EMIT',
-                         additional_outputs = ['EMIT','ROT_ANG'], **kwargs)
+                         additional_outputs = ['EMIT','IXXI','IXPXPI','IXXPI','ROT_ANG'], **kwargs)
         
     
     def __call__(self, controller):
@@ -230,10 +230,13 @@ class Emittance(AWAScreen):
                                                                   self.drift)]
                 
             else:
-                emittances += [np.nan]
+                emittances += [np.nan*np.ones(4)]
                 rotation_angles += [np.nan]
 
-        emittances = np.array(emittances).reshape(-1,1)
+        emittances = np.array(emittances).reshape(-1,4)
+        #cut out any meaurements of the emittance over 5e-7
+        #emittances = np.where(emittances > 5e-7, np.nan, emittances).reshape(-1,1)
+        
         rotation_angles = np.array(rotation_angles).reshape(-1,1)
 
         scalar_data = np.hstack([scalar_data, emittances, rotation_angles])

AWA/test_script.py
@@ -28,24 +28,29 @@ def main():
     emittance_obs = observations.Emittance(0.002, 
                                            2.84, 
                                            1086, 
-                                           n_samples = 5)
+                                           n_samples = 5,
+                                           target_charge = -6.5e-9,
+                                           charge_deviation = 0.05)
     
     #scan_vals = np.array(((8.3, 1.2),
     #                      (8.3, 1.4),
     #                      (8.3, 1.6)))
-    #scan_vals = np.array(((7.2, 1.0))).reshape(1,-1)
+    #scan_vals1 = np.array(((8.84, 0.37, 0.0))).reshape(1,-1)
     scan_vals = np.random.rand(10,3)
+    #scan_vals = np.linspace(-0.05,0.3,10).reshape(-1,1)
     
     param = c.get_named_parameters(['FocusingSolenoid',
                                     'MatchingSolenoid',
                                     'DQ5'])
+    
+    #param = c.get_named_parameters(['DQ5'])
     pre_observation_func = pre_observation.Sleep(5)
     #pre_observation_func = pre_observation.KeyPress()
    
     if 0:
         sampling = sample.Sample(param,
                                  [screen_obs],
-                                 c, scan_vals,
+                                 c, scan_vals1,
                                  normalized = False,
                                  pre_observation_function = pre_observation_func)
         sampling.run()
@@ -69,15 +74,15 @@ def main():
                                        c,
                                        [roi_constraint],
                                        n_steps = 50,
-                                       sigma = 100.0)
-    if 0:
+                                       sigma = 100)
+    if 1:
         bexp.run()
         #model = bexp.create_model()
         #bexp.get_acq(model, True)
     
     #c.load_data('data/good_3_parameter_exploration.pkl')
-    #print(c.data[['MatchingSolenoid','FocusingSolenoid','LinacPhase',
-    #              'EMIT','IMGF','ROT_ANG']])
+    print(c.data[['MatchingSolenoid','FocusingSolenoid','LinacPhase',
+                  'EMIT','IMGF','ROT_ANG']])
     #print(c.data)
     model = bexp.create_model()
     models = model.models

accelerator_control/algorithms/explore.py
@@ -96,7 +96,9 @@ class BayesianExploration(bayesian_algorithm.BayesianAlgorithm):
         acq = self.get_acq(model)
         bounds = torch.stack([torch.zeros(self.n_parameters),
                               torch.ones(self.n_parameters)])
+        return self.optimize(acq, bounds)
     
+    def optimize(self, acq, bounds):
         #sometimes this can fail due to PSDError, if it does try doing it again with
         #one less point
         try:
@@ -107,9 +109,7 @@ class BayesianExploration(bayesian_algorithm.BayesianAlgorithm):
             self.logger.warning('Non PSD matrix in model, try to recreate with one less point')
             model = self.create_model(True)
             acq = self.get_acq(model)
-            candidate, acq_value = optimize_acqf(acq, bounds = bounds,
-                                                 q=1, num_restarts = 20,
-                                                 raw_samples = 20)
+            candidate = self.optimize(acq, bounds)
             
         return candidate
     